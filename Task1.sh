3e4a1fd82c4da255f498211fc4ee18f001b0f7a6
Aswin-tech-07 <97091541+Aswin-tech-07@users.noreply.github.com>
2024-03-10 14:24:19 +0530

    Update Task1.sh
    Completed day 1 task

--- Task1.sh
+++ Task1.sh
@@ -1,1 +1,16 @@
-testing
+##Task 1  09-03-2024
+## Title -> Create 20 files with .txt extensions and rename the first 5 files to .yml extension
+## Print the latest created top 5 files among the total no of files
+
+##creating executable file for first day task
+touch task1executable.sh
+##assign executable permission for that script
+chmod +x task1executable.sh
+## create 20  .txt files
+echo "touch file{1..20}.txt" > task1executable.sh 
+##rename first 5 .txt file to .yml
+echo 'for i in {1..5}; do mv "file${i}.txt" "file${i}.yml";done' >> task1executable.sh
+
+##printing 
+echo 'ls *.yml' >> task1executable.sh
+
